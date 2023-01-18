dfc3a338791c6942e475f4f8273f564708862cf4
seccortessa <seccortessa@unal.edu.co>
2023-01-17 23:46:52 -0500

    day 3 completed

--- day3Project.py
+++ day3Project.py
@@ -28,4 +28,25 @@ print("Your mission is to find the treasure.")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-print("You're" + 'at a cross road. Where do you want to go? Type "left" or "right" ')
+print("You're" + 'at a cross road. Where do you want to go? Type "left" or "right"\n ')
+
+choice = input()
+
+if choice == "right":
+    print("Game over")
+elif choice == "left":
+    choice = input("You have reached to a lake, now decide if wait or take a boat: ")
+    if choice == "swim":
+        print("Gameover")
+    elif choice == "wait":
+        choice = input("Noe you must choice a dor. there is 3: Red, Blue and Yellow: ")
+        if choice == "red":
+            print("Game over")
+        elif choice == "blue":
+            print("Game over")
+        elif choice == "yellow":
+            print("You win!!")
+            
+        
+        
+    
