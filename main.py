8fc88003a34a3a24117b70af548c0c46effb2560
seccortessa <seccortessa@unal.edu.co>
2023-01-26 12:55:26 -0500

    8.1 challenge completed

--- main.py
+++ main.py
@@ -1,43 +1,11 @@
-def turn_right():
-    turn_left()    
-    turn_left()    
-    turn_left()
 
-def check_coll():
-    while front_is_clear():
-        move()
-        if not wall_on_right():
-            turn_right()
-            pass
-    if wall_on_right():
-        turn_left()
-    while not (front_is_clear() and wall_on_right()):
-        turn_right()
-        
-        
-def move_to_wall():
-    while front_is_clear():
-        move()
-        
-        
-def go_and_right():
-    while not wall_on_right():         
-        turn_right()
-        move()
-        check_coll()
+def paint_calc(height, width, cover):
+    calculation = (height * width) / cover
+    calculation = round(calculation)
+    print(calculation + 1)
 
-        
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
 
-while not at_goal():
-    if not front_is_clear():
-        turn_right()
-    else:
-        move()
-    while wall_on_right():
-        if not front_is_clear():
-            turn_left()
-        else:
-            move()
-    turn_right()
-    
-    if  ( not last_four[0]) and (not last_four[1]) and (not last_four[2]) and (not last_four[3])
