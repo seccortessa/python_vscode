24f91264dd297b64d91f552cfad82d0faf0a04a1
seccortessa <seccortessa@unal.edu.co>
2023-02-22 09:34:01 -0500

    First Commit for blackJack

---
+++ day11BlackjackGame.py
@@ -0,0 +1,37 @@
+
+
+
+# we're gonna building a blackjack game
+
+"""game rules:
+1. Dealer hand himself and player a card, showing the value, and then one more card, but this one is hidden.
+2. the goals is achieve 21 as scoring, so the player can take a card from maze, or stay 
+3. if you have less than 17, you must take a card.
+3. if one reaches more than 21, loses
+4. if none reaches the 21, the player with largest number under 21 wins.
+
+* A counts as 11 or as 1
+* J,  Q and K count as 10
+* the remaining cards count as it's own number.
+"""
+    
+import random
+
+
+cards = [11, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9, 10, 10, 10, 10]
+
+
+def myfun():
+    a["nuevo"] = [1,2]
+
+a = {
+    1: "hola",
+    2: True,
+    3: 4
+}
+
+print("Antes de:", a)
+
+myfun()
+
+print("Despues de:" ,a)
